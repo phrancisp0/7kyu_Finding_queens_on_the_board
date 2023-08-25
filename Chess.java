93ecb41cb0b96cb52c9c9b06fb55d067b263593d
Francis Patawah <phrancisp0@gmail.com>
2023-08-25 14:16:21 -0400

    7kyu finding queens on the board

---
+++ Chess.java
@@ -0,0 +1,65 @@
+/*
+
+Your task is to find the maximum number of queens that can be put on the board so that there would be one single unbeaten square (ie. threatened by no queen on the board).
+
+The Queen can move any distance vertically, horizontally and diagonally.
+
+Input
+
+The queens(n) function takes the size of the chessboard.
+
+n
+∈
+Z
+n∈Z, so it can be negative, and values can go up to 
+14
+1
+1000
+141 
+1000
+ .
+
+Output
+
+The maximum number of queens to leave one single unbeaten square
+Return 0 if n is negative.
+Examples
+
+n
+=
+4
+→
+6
+ 
+q
+u
+e
+e
+n
+s
+n=4→6 queens
+n
+=
+5
+→
+12
+ 
+q
+u
+e
+e
+n
+s
+n=5→12 queens
+Good luck )
+
+*/
+
+import java.math.BigInteger;
+public class Chess {
+  public static BigInteger queens(BigInteger n) { 
+    if(n.compareTo(BigInteger.valueOf(0))<1)return BigInteger.valueOf(0);
+    BigInteger c = n.multiply(BigInteger.valueOf(3)).subtract(BigInteger.valueOf(2));
+    return n.pow(2).subtract(c);
+  }
+} 
